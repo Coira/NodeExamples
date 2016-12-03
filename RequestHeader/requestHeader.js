729d386bdee7dceea550f73ac92c578da80ea823
Coira <coira.ells@gmail.com>
2016-12-03 23:35:29 +0000

    Request Header Parser Microservice

--- RequestHeader/requestHeader.js
+++ RequestHeader/requestHeader.js
@@ -4,19 +4,21 @@ User Story: I can get the IP address, language and operating system for my brows
 https://cryptic-ridge-9197.herokuapp.com/api/whoami/
 
 */
-
+/*
+/\((.*)\)/g[1]
+*/
 var os = require('os');
 
 module.exports = function(app) {
     app.get('/node-examples/request-header', function(req, res) {
-        //console.log(req.ip);
+
         res.json({
             "ipaddress": req.headers['x-forwarded-for'] || 
                 req.connection.remoteAddress || 
                 req.socket.remoteAddress ||
                 req.connection.socket.remoteAddress,
             "language": req.acceptsLanguages()[0],
-            "software": os.type() + " " + os.release()
+            "software": req.headers['user-agent'].match(/\((.*?)\)/)[1]
         });
     });
 };
